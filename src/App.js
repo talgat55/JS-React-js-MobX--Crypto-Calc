e20dc5988ddc1f51c55e04c9bc406991d5ef066c
talgat55 <lightxdesign55@gmail.com>
2020-04-14 21:56:57 +0600

    return

--- src/App.js
+++ src/App.js
@@ -1,26 +1,50 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {useState, useEffect} from 'react';
+import styled from 'styled-components';
+import axios from 'axios';
+import {Container, Grid,  Segment } from 'semantic-ui-react';
+import 'semantic-ui-css/semantic.min.css';
+import TableComponent from './components/Table/Table';
+import Coverter from "./components/Coverter/Coverter";
+const App = () => {
+    const currencyOptions = [
+        {key: 'af', value: 'af', text: 'Afghanistan'},
+        {key: 'ax', value: 'ax', text: 'Aland Islands'},
+    ];
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
 
+    return (
+        <SelectComponent>
+            <Container>
+                <Grid>
+                    <Grid.Row>
+                        <Grid.Column width={9}>
+                            <TableComponent />
+                        </Grid.Column>
+                        <Grid.Column width={7}>
+                            <Segment>
+                                <Coverter
+                                    currencyOptions={currencyOptions}
+                                />
+                            </Segment>
+                        </Grid.Column>
+                    </Grid.Row>
+                </Grid>
+            </Container>
+        </SelectComponent>
+    );
+};
+const SelectComponent = styled.div`
+  margin-top: 50px;
+  .currency-group{
+    margin-bottom: 20px;
+  }
+  .selection{
+    margin-left: 10px;
+  }
+  .image{
+    max-width: 15px!important;
+  }
+  
+
+`;
 export default App;
---
+++ src/components/Table/Table.jsx
@@ -0,0 +1,36 @@
+import React from 'react';
+import {Table }   from 'semantic-ui-react';
+import {inject, observer} from "mobx-react";
+@inject('currenciesStore')
+@observer
+export default  class extends React.Component {
+        render(){
+            return  (
+                <>
+                    <Table celled>
+                        <Table.Header>
+                            <Table.Row>
+                                <Table.HeaderCell>Full Name</Table.HeaderCell>
+                                <Table.HeaderCell>Name</Table.HeaderCell>
+                                <Table.HeaderCell>Price</Table.HeaderCell>
+                                <Table.HeaderCell>24 hours</Table.HeaderCell>
+                            </Table.Row>
+                        </Table.Header>
+
+                        <Table.Body>
+                            {
+                                data.map(item =>(
+                                    <Table.Row>
+                                        <Table.Cell>{item.name} <img className="image" src={item.imageUrl}  alt="Icon" /></Table.Cell>
+                                        <Table.Cell>{item.fullName}</Table.Cell>
+                                        <Table.Cell>${item.price}</Table.Cell>
+                                        <Table.Cell>${item.volume24Hour}</Table.Cell>
+                                    </Table.Row>
+                                ))
+                            }
+                        </Table.Body>
+                    </Table>
+                </>
+            )
+        }
+}
